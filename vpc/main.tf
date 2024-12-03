f43d547649940695b60b4839591206707df8498a
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-03 13:42:53 +0530

    check_if_job_exist_or_not

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,15 +1,34 @@
-resource "aws_vpc" "auto_vpc" {
+resource "aws_vpc" "vpc-1" {
   cidr_block = "10.0.0.0/16"
   tags       = merge(local.common_tags,{ Name= "${var.env}-vpc" })
 }
 
+resource "aws_vpc" "vpc-2" {
+  cidr_block = "10.0.0.0/8"
+  tags       = merge(local.common_tags,{ Name= "${var.env}-vpc" })
+}
 resource "aws_subnet" "public_subnet" {
-  count      = length(var.cidr_block)
-  vpc_id     = aws_vpc.auto_vpc.id
-  cidr_block = var.cidr_block[count.index]
+  count      = length(var.cidr_block_1)
+  vpc_id     = aws_vpc.vpc-1.id
+  cidr_block = var.cidr_block_1[count.index]
   availability_zone = var.availability_zones[count.index]
 
-  tags       = merge(local.common_tags,{ Name= "${var.env}-Subnet-${count.index + 1}" })
+  tags       = merge(local.common_tags,{ Name= "${var.env}-Public_Subnet-${count.index + 1}" })
 
 }
 
+resource "aws_subnet" "private_subnet" {
+  count      = length(var.cidr_block_2)
+  vpc_id     = aws_vpc.vpc-2.id
+  cidr_block = var.cidr_block_2[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags       = merge(local.common_tags,{ Name= "${var.env}-Private_Subnet-${count.index + 1}" })
+
+}
+
+resource "aws_vpc_peering_connection" "auto_peer" {
+  peer_owner_id = data.aws_caller_identity.current_account.account_id
+  peer_vpc_id = aws_vpc.vpc-1.id
+  vpc_id      = aws_vpc.vpc-2.id
+}
--- vpc/vars.tf
+++ vpc/vars.tf
@@ -1,8 +1,11 @@
 variable "env" {
   default = "dev"
 }
-variable "cidr_block" {
+variable "cidr_block_1" {
   default = ["10.0.0.0/17", "10.0.128.0/17"]
+}
+variable "cidr_block_2" {
+  default = ["10.0.0.0/9", "10.128.0.0/9"]
 }
 variable "availability_zones" {
   default = ["us-east-1a", "us-east-1b"]
