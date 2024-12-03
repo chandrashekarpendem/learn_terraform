10a247784d38dda5460812c3b800e958359f0277
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2024-12-03 13:16:55 +0530

    check_if_job_exist_or_not

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,6 +7,7 @@ resource "aws_subnet" "public_subnet" {
   count      = length(var.cidr_block)
   vpc_id     = aws_vpc.auto_vpc.id
   cidr_block = var.cidr_block[count.index]
+  availability_zone = var.availability_zones[count.index]
 
   tags       = merge(local.common_tags,{ Name= "${var.env}-${count.index + 1}" })
 
